b6cf25b045d75ef4c6d80476cbf657c89550e019
unknown <AsishS@AsishS.talentica-all.com>
2015-08-18 13:04:49 +0530

    Signed-off-by: unknown <AsishS@AsishS.talentica-all.com>

---
+++ JUnitProjects/src/main/java/org/gradle/JunitTest2.java
@@ -0,0 +1,10 @@
+package org.gradle;
+
+import org.junit.Test;
+
+public class JunitTest2 {
+	@Test(expected = ArithmeticException.class)  
+	public void divisionWithException() {  
+	  int i = 1/0;
+	} 
+}
---
+++ JUnitProjects/src/main/java/org/gradle/TestJunit.java
@@ -0,0 +1,14 @@
+package org.gradle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJunit {
+
+	   @Test
+	   public void testAdd() {
+	      String str= "Junit is working fine";
+	      assertEquals("Junit is working fine",str);
+	   }
+}
